6531a68d67aae3a0e42e4dd1a33ec108c3f75176
micha003 <michael.vorogushyn@wbgym.de>
2023-03-22 22:05:09 +0100

    added todos`

--- shapes/script_kreis.js
+++ shapes/script_kreis.js
@@ -3,13 +3,13 @@
 function calculateArea() {
   var radius = document.getElementById("radius").value;
   var area = Math.PI * radius * radius;
-  document.getElementById("A").innerHTML = area;
+  document.getElementById("A").innerHTML = area; // TODO: add more text and the unit
 }
 
 function calculateUmfang() {
   var radius = document.getElementById("radius").value;
   var umfang = 2 * Math.PI * radius;
-  document.getElementById("u").innerHTML = umfang;
+  document.getElementById("u").innerHTML = umfang; // TODO: add more text and the unit
 }
 
 function calculate() {
--- shapes/script_quadrat.js
+++ shapes/script_quadrat.js
@@ -1,19 +1,19 @@
 function calculateArea() {
   var side = document.getElementById("side").value;
   var area = side * side;
-  document.getElementById("A").innerHTML = area;
+  document.getElementById("A").innerHTML = area; // TODO: add more text and the unit
 }
 
 function calculateUmfang() {
   var side = document.getElementById("side").value;
   var umfang = 4 * side;
-  document.getElementById("u").innerHTML = umfang;
+  document.getElementById("u").innerHTML = umfang; // TODO: add more text and the unit
 }
 
 function calculateDiagonal() {
   var side = document.getElementById("side").value;
   var diagonal = Math.sqrt(2) * side;
-  document.getElementById("diagonal").innerHTML = diagonal;
+  document.getElementById("diagonal").innerHTML = diagonal; // TODO: add more text and the unit
 }
 
 function calculate() {
